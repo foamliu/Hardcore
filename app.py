e5da89e08767a7818479d814f1b24cc5a961cba9
Yang Liu <liuyang12@focusmedia.cn>
2019-05-21 18:27:44 +0800

    update

---
+++ app.py
@@ -0,0 +1,27 @@
+# flask_web/app.py
+
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+
+def create_app(config_name):
+    app = Flask(config_name, static_url_path="", static_folder="static")
+    bootstrap.init_app(app)
+    # Bootstrap(app)
+    return app
+
+
+app = create_app(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/alg_top')
+def alg_top():
+    return render_template('course.html')
